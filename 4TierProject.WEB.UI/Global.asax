854bcc27ca04a9e7e5f9e6ff991d9eba11a44612
KEMAL KAYAHAN <m.kemal.kayahan@hotmail.com>
2018-05-19 20:03:50 +0300

    project structure

---
+++ 4TierProject.WEB.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_4TierProject.WEB.UI.MvcApplication" Language="C#" %>
---
+++ 4TierProject.WEB.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4TierProject.WEB.UI.Startup))]
+namespace _4TierProject.WEB.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
